d30935d02a3d4c96d4c786a6fccd177ffa9d140b
ParvPatel01 <patel.parv1@northeastern.edu>
2025-04-16 22:55:19 -0400

    Implementing Node actor

--- src/main/scala/actors/Broker.scala
+++ src/main/scala/actors/Broker.scala
@@ -8,8 +8,10 @@ import akka.cluster.pubsub.DistributedPubSubMediator.{SubscribeAck, Subscribe}
 object Broker {
   sealed trait BrokerMessage
   case class AddTransaction(transaction: Transaction) extends BrokerMessage
-  case object GetTransaction extends BrokerMessage
+  case object GetTransactions extends BrokerMessage
+  case class DiffTransaction(transactions: List[Transaction]) extends BrokerMessage
   case object Clear extends BrokerMessage
+
 
   val props: Props = Props(new Broker)
 }
@@ -24,13 +26,19 @@ class Broker extends Actor with ActorLogging {
       pending = transaction :: pending
       log.info(s"Added $transaction to pending Transaction")
     }
-    case GetTransaction => {
+    case GetTransactions => {
       log.info(s"Getting Pending Transactions")
       sender() ! pending
+    }
+    case DiffTransaction(externalTransactions) => {
+      pending = pending diff externalTransactions
     }
     case Clear => {
       pending = List()
       log.info("Clear Pending transaction List")
+    }
+    case SubscribeAck(Subscribe("transaction", None, `self`)) => {
+      log.info("subscribing")
     }
   }
 }
